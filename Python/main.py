e0668da5a6220b9537fb8d3c820a8b1f46746f57
Matthew Wong <53923157+matthewwong212@users.noreply.github.com>
2022-05-21 15:39:03 -0700

    Easier to change between CuPy and NumPy

--- Python/main.py
+++ Python/main.py
@@ -7,13 +7,12 @@
 #  2 = Right eye monoscopic
 #  3 = Top-bottom
 #  4 = Row interleaved
-# SELECTION (MODE)
 MODE = 3
 
 # POLARITY SWAP
 #  0 = Default
 #  1 = Swapped
-# SELECTION (POLARITY)
+#  SELECTION (POLARITY)
 POLARITY = 0
 
 # IMAGE CORRECTION
@@ -34,18 +33,25 @@ VIDEO_RIGHT = 'right_camera.mp4'
 # DEBUG MESSAGES
 VERBOSE = False
 
+# Set CuPy or NumPy Execution. Internal testing only.
+use_cupy = True
+
 # OLD: Previously assumed stereo vision, half resolution, slight disparity
 #  This may still be used by feeding side-by-side back to the beginning
 VIDEO = 'city_video_1080p.mp4'
 
 
 from sre_constants import SUCCESS
-import cupy as cp # <----------------------------------------	Bug here: cupy or numpy
 import sys
 sys.path.append('/usr/local/lib/python3.8/site-packages') # Allow Python3.8 to refer to OpenCV4.5.1 install library
 import cv2
 import argparse_file
+if use_cupy:
+    import cupy as np
+else:
+    import numpy as np
 
+# Gets parsed command line arguments from argparse_file
 parser = argparse_file.create_parser()
 args = parser.parse_args()
 
@@ -53,6 +59,7 @@ args = parser.parse_args()
 VIDEO_LEFT = args.leftCamera
 VIDEO_RIGHT = args.rightCamera
 
+# Set debugging messages
 if args.verbose:
     if args.verbose == "True":
         print("Showing debugging print statements")
@@ -61,6 +68,7 @@ if args.verbose:
         print("Debugging print statements disabled")
         VERBOSE = False
 
+# Set chosen mode
 if args.mode:
     if (args.mode == 1):
         print("Mode set to Left Camera")
@@ -73,7 +81,7 @@ if args.mode:
 
     MODE = args.mode
 
-# Argument passed as string, since options are 0 or 1.
+# Set polarity.  Argument passed as string, since options are 0 or 1.
 if args.polarity:
     if (args.polarity == "0"):
         print("Default polarity")
@@ -82,7 +90,7 @@ if args.polarity:
 
     POLARITY = int(args.polarity)
 
-# Argument passed as string, since options include 0
+# Set image correction mode.  Argument passed as string, since options include 0
 if args.imageCorrection:
     if (args.imageCorrection == "0"):
         print("Unaltered image")
@@ -93,24 +101,22 @@ if args.imageCorrection:
 
     IMCORR_MODE = int(args.imageCorrection)
 
-# Argument passed as string, since 0 option is possible
+# Set saturation scale.  Argument passed as string, since 0 option is possible
 if args.saturationScale:
     print("Saturation scale set to: ", args.saturationScale)
 
     SAT_SCALE = float(args.saturationScale)
 
 
-
 # BEGIN MAIN EXECUTION
 # Place odd rows from left above even rows from right
 def top_bottom(left_in, right_in, pol):
     if verbose: print('Made it to top-bottom')
-    num_rows, num_cols, num_ch = cp.shape(left_in)
+    num_rows, num_cols, num_ch = np.shape(left_in)
     rows = int(num_rows / 2)
     if verbose: print('Before CuPy')
-    out = cp.zeros_like(left_in) # <--------------------------	Bug here: cupy version (need 10.2)
+    out = np.zeros_like(left_in)
     if verbose: print('Splitting...')
-    if verbose: print(type(left_in))
     if pol:
         # Polarity is swapped
         out[0:rows,:] = right_in[::2,:]
@@ -122,30 +128,29 @@ def top_bottom(left_in, right_in, pol):
     if verbose: print('Ready to display')
     display('Top-bottom', out)
 
-# Side-by-side optional
-
 # Interleave odd rows from left and even rows from right
 def row_interleaved(left_in, right_in, pol):
-    out = cp.zeros_like(left_in)
+    out = np.zeros_like(left_in)
     if pol:
         # Polarity is swapped
         out[::2,:] = right_in[::2,:]
         out[1::2,:] = left_in[1::2,:]
     else:
         # Default polarity
-        out[::2,:] = cp.array(left_in[::2,:])
-        out[1::2,:] = cp.array(right_in[1::2,:])
+        out[::2,:] = np.array(left_in[::2,:])
+        out[1::2,:] = np.array(right_in[1::2,:])
     display('Row interleaved', out)
 
 def original(left_in, right_in, pol):
-    out = cp.zeros_like(cp.hstack((left_in, right_in)))
+    out = np.zeros_like(np.hstack((left_in, right_in)))
     if pol:
         # Polarity is swapped
-        out = cp.hstack((right_in, left_in))
+        out = np.hstack((right_in, left_in))
     else:
         # Default polarity
-        out = cp.hstack((left_in, right_in))
+        out = np.hstack((left_in, right_in))
     display('Original (Side-by-side)', out)
+
 
 def display(window, output):
     # Fullscreen options for testing
@@ -153,29 +158,33 @@ def display(window, output):
     #cv2.setWindowProperty(window, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     # Result 0 -- Unmodified output (Default)
-    cp_out = cp.uint8(output.get())         # For CuPy, Comment out for NumPy
-    #cp_out = cp.uint8(output)               # For NumPy, Comment out for CuPy
-    result = cp_out[:, :, [0, 1, 2]]
+    # Differing lines for CuPy vs NumPy
+    if use_cupy:
+        np_out = np.uint8(output.get())
+    else:
+        np_out = np.uint8(output)
+
+    result = np_out[:, :, [0, 1, 2]]
     if verbose: print('Attempting to display...')
 
     # Result 1 -- Sepia toned output
     if IMCORR_MODE==1:
-        sepia_kernel = cp.float64([[0.272, 0.534, 0.131],
+        sepia_kernel = np.float64([[0.272, 0.534, 0.131],
                                 [0.349, 0.686, 0.168],
                                 [0.393, 0.769, 0.189]])
-        cp_out_float = cp.float64(output.get())
-        result_2s = cv2.transform(cp_out_float, sepia_kernel)
-        #result[cp.where(cp.asarray(result) > 255)] = 255
-        result_2c = cp.clip(result_2s, 0, 255)
-        result_2a = cp.array(result_2c, dtype=cp.uint8)
-        result = cp.uint8(result_2a.get())
+        np_out_float = np.float64(output.get())
+        result_2s = cv2.transform(np_out_float, sepia_kernel)
+        #result[np.where(np.asarray(result) > 255)] = 255
+        result_2c = np.clip(result_2s, 0, 255)
+        result_2a = np.array(result_2c, dtype=np.uint8)
+        result = np.uint8(result_2a.get())
 
     # Result 2 -- Saturation adjustment
     if IMCORR_MODE==2:
-        out_hsv = cv2.cvtColor(cp_out, cv2.COLOR_BGR2HSV).astype("float64")
+        out_hsv = cv2.cvtColor(np_out, cv2.COLOR_BGR2HSV).astype("float64")
         (h,s,v) = cv2.split(out_hsv)
         s = s*SAT_SCALE
-        s = cp.clip(s, 0, 255)
+        s = np.clip(s, 0, 255)
         out_hsv = cv2.merge([h,s,v])
         result = cv2.cvtColor(out_hsv.astype("uint8"), cv2.COLOR_HSV2BGR)
 
@@ -201,12 +210,12 @@ while(L_capture.isOpened() and R_capture.isOpened()):
     R_success, R_frame = R_capture.read()
     if L_success and R_success:
         if verbose: print('Success')
-        num_L_rows, num_L_cols, num_ch = cp.shape(L_frame)
-        num_R_rows, num_R_cols, num_ch = cp.shape(R_frame)
+        num_L_rows, num_L_cols, num_ch = np.shape(L_frame)
+        num_R_rows, num_R_cols, num_ch = np.shape(R_frame)
         
         # Read into CuPY (Comment out block when using NumPy)
-        L_frame = cp.asarray(L_frame)
-        R_frame = cp.asarray(R_frame)
+        L_frame = np.asarray(L_frame)
+        R_frame = np.asarray(R_frame)
         #cols = int(num_cols / 2)
         # Mode select
         if MODE == 1:
